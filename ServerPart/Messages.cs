26f8dbf5301367fbd76eee66036bd4b152dc9475
Иван Коваленко <ivan.kovalenko8080@gmail.com>
2024-03-25 23:54:10 +0100

    Server Rebuild

--- ServerPart/Messages.cs
+++ ServerPart/Messages.cs
@@ -1,16 +1,19 @@
 ﻿using Newtonsoft.Json;
 
-/// <summary>
-/// An implemintation of message 
-/// </summary>
-public class Message
+namespace Connect.message
 {
-    [JsonProperty("datetime")]
-    public string MessageDateTime { get; set; } = string.Empty;
-    [JsonProperty("login")]
-    public string Login { get; set; } = string.Empty;
-    [JsonProperty("content")]
-    public string Content { get; set; } = string.Empty;
-    [JsonProperty("type")]
-    public string MessageType { get; set; } = string.Empty;
+    /// <summary>
+    /// An implemintation of message 
+    /// </summary>
+    public class Message
+    {
+        [JsonProperty("datetime")]
+        public string MessageDateTime { get; set; } = string.Empty;
+        [JsonProperty("login")]
+        public string Login { get; set; } = string.Empty;
+        [JsonProperty("content")]
+        public string Content { get; set; } = string.Empty;
+        [JsonProperty("type")]
+        public string MessageType { get; set; } = string.Empty;
+    }
 }
--- ServerPart/User.cs
+++ ServerPart/User.cs
@@ -1,16 +1,19 @@
 ﻿using Newtonsoft.Json;
 
-/// <summary>
-/// An implementation of user
-/// </summary>
-public class User
+namespace Connect.user
 {
-    [JsonProperty("username")]
-    public string UserName { get; set; } = string.Empty;
-    [JsonProperty("login")]
-    public string Login { get; set; } = string.Empty;
-    [JsonProperty("password")]
-    public string Password { get; set; } = string.Empty;
-    [JsonProperty("aboutme")]
-    public string AboutMe { get; set; } = string.Empty;
+    /// <summary>
+    /// An implementation of user
+    /// </summary>
+    public class User
+    {
+        [JsonProperty("username")]
+        public string UserName { get; set; } = string.Empty;
+        [JsonProperty("login")]
+        public string Login { get; set; } = string.Empty;
+        [JsonProperty("password")]
+        public string Password { get; set; } = string.Empty;
+        [JsonProperty("aboutme")]
+        public string AboutMe { get; set; } = string.Empty;
+    }
 }
